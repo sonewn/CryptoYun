78233850e3bb5e1c8e380710d33e823ea8a8158f
soy <89831708+sonewn@users.noreply.github.com>
2025-11-02 21:33:41 +0900

    업비트 자산현황 조회 기능 및 api collector 반영

---
+++ src/ingestion/upbit_collector.py
@@ -0,0 +1,65 @@
+import os
+import time
+import requests
+import jwt
+import yaml
+from datetime import datetime, timedelta
+from typing import Optional
+import pandas as pd
+import uuid
+
+class UpbitCollector:
+    def __init__(self, config_path: str = "config/api_keys.yaml"):
+        self.config = self._load_config(config_path)
+        self.access_key = self.config["upbit"]["access_key"]
+        self.secret_key = self.config["upbit"]["secret_key"]
+        self.base_url = self.config["upbit"]["base_url"]
+
+    def _load_config(self, path: str):
+        with open(path, "r") as f:
+            return yaml.safe_load(f)
+    
+    def _create_headers(self, query: Optional[dict] = None):
+        payload = {
+            "access_key": self.access_key,
+            "nonce": str(uuid.uuid4()),
+        }
+
+        if query != None:
+            import hashlib
+            import urllib.parse
+
+            query_string = urllib.parse.urlencode(query).encode("utf-8")
+            m = hashlib.sha512()
+            m.update(query_string.encode("utf-8"))
+            query_hash = m.hexdigest()
+            payload["query_hash"] = query_hash
+            payload["query_hash_alg"] = "SHA512"
+        
+        jwt_token = jwt.encode(payload, self.secret_key, algorithm="HS256")
+        auth = f"Bearer {jwt_token}"
+
+        return {"Authorization": auth}
+    
+    def get_my_asset(self):
+        "보유 자산 현황 조회"
+        url = f"{self.base_url}/accounts"
+        headers = self._create_headers()
+        res = requests.get(url, headers=headers)
+        res.raise_for_status()
+        return pd.DataFrame(res.json())
+    
+    def save_file(self, df:pd.DataFrame, category: str):
+        "수집 데이터 파일 저장 - 추후 확장자 고민 필요"
+        os.makedirs("data/raw", exist_ok=True)
+        file_path = f"data/raw/{category}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.csv"
+        df.to_csv(file_path, index=False)
+        print(f"Saved {category} data succesfully --> {file_path}")
+
+
+if __name__ == "__main__":
+    collector = UpbitCollector()
+
+    # 1️⃣ 나의 자산현황
+    balance_df = collector.get_my_asset()
+    collector.save_to_csv(balance_df, "my_asset")
